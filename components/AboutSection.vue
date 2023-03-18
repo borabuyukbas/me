a176e89372b245baa69dbed6259c8348a902e133
Bora <borabuyukbas@hotmail.com>
2023-03-19 00:36:02 +0300

    fix: margins

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -18,8 +18,8 @@ const language_skills = [
 </script>
 
 <template>
-  <div class="flex lg:flex-row flex-col mb-4">
-    <div class="flex-shrink-0 flex-basis-60% mb-2 lg:mb-0 mr-0 lg:mr-2 lg:mb-2">
+  <div class="flex lg:flex-row flex-col mb-16">
+    <div class="flex-shrink-0 flex-basis-60% mb-2 lg:mb-0 mr-0 lg:mr-8 mb-8">
       <h3 class="text-2xl font-bold mb-2">
         {{ $t("about_title") }}
       </h3>
