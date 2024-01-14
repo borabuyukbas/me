91e2de5e52b23139c58438545f7362296198d094
Bora <borabuyukbas@hotmail.com>
2024-01-14 15:55:26 +0100

    feat(header): made navbar responsive on mobile

--- components/Header.vue
+++ components/Header.vue
@@ -24,28 +24,53 @@ function flagName(country_code: string) {
   }
 }
 
+const navActive = useState("navActive", () => false)
+
+useNuxtApp().$router.afterEach(() => {
+  navActive.value = false;
+})
+
 </script>
 
 <template>
   <div>
     <nav class="flex items-center justify-between">
       <div class="flex items-center">
-        <NuxtLink
-          class="pr-4 py-1 text-neutral-500 hover:text-neutral-700 dark:hover:text-neutral-300 transition-colors font-bold uppercase"
-          active-class="text-neutral-700 dark:text-neutral-300"
-          href="/"
+        <div class="block lg:hidden">
+          <button
+            class="mr-4 flex justify-center items-center h-8 w-8 hover:bg-light-900 dark:hover:bg-dark-300 transition-colors cursor-pointer rounded-2"
+            :class="navActive ? 'bg-light-900 dark:bg-dark-300' : 'bg-light-700 dark:bg-dark-500'"
+            @click="navActive = !navActive"
+          >
+            <div
+              :class="!navActive ? 'ion:navicon' : 'ion:close'"
+            />
+          </button>
+        </div>
+        <div
+          class="lg:block"
+          :class="navActive ? 'block' : 'hidden'"
         >
-          {{ t("pages_home") }}
-        </NuxtLink>
-        <NuxtLink
-          class="pr-4 py-1 text-neutral-500 hover:text-neutral-700 dark:hover:text-neutral-300 transition-colors font-bold uppercase"
-          active-class="text-neutral-700 dark:text-neutral-300"
-          href="/projects"
-        >
-          {{ t("pages_projects") }}
-        </NuxtLink>
+          <NuxtLink
+            class="pr-4 py-1 text-neutral-500 hover:text-neutral-700 dark:hover:text-neutral-300 transition-colors font-bold uppercase"
+            active-class="text-neutral-700 dark:text-neutral-300"
+            href="/"
+          >
+            {{ t("pages_home") }}
+          </NuxtLink>
+          <NuxtLink
+            class="pr-4 py-1 text-neutral-500 hover:text-neutral-700 dark:hover:text-neutral-300 transition-colors font-bold uppercase"
+            active-class="text-neutral-700 dark:text-neutral-300"
+            href="/projects"
+          >
+            {{ t("pages_projects") }}
+          </NuxtLink>
+        </div>
       </div>
-      <div class="flex items-center">
+      <div
+        class="lg:flex items-center"
+        :class="!navActive ? 'flex' : 'hidden'"
+      >
         <a
           v-for="locale in localeCodes"
           :key="'localeButton_' + locale"
